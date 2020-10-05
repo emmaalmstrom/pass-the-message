39db619f2e369d928e4e4b4f28cb0559763e5b9c
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-10-05 13:01:22 +0200

    ja

---
+++ script.js
@@ -0,0 +1,12 @@
+const textytan = document.getElementById("textyta")
+const knapp = document.getElementById("knapp")
+const outputt = document.getElementById("output4")
+
+console.log (textytan.value)
+console.log("hej hej")
+
+knapp.onclick = klick
+
+function klick(){
+    console.log (textytan.value)
+}
